b2ef55237082a29841bbd0dea84c37e7d8df4ca9
Fayeq Bahram <fayeq.bahram@gmail.com>
2026-01-17 23:51:45 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const menuBtn = document.getElementById("menuBtn")
+const navBar  = document.getElementById("navBar");
+
+ menuBtn.addEventListener("click", ()=>{
+   navBar.classList.toggle("new-menu");
+ })
+
+//_____________  Sales Page / Sell button___________
+
+
